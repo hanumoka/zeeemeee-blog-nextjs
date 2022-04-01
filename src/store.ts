dcbd37e5ab5f3bea2620050159f735d585b67ca0
amagramer <amagramer@gmail.com>
2022-04-01 14:17:09 +0900

    feat: zustand예제 완성

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,22 @@
-import create from 'zustand';
+import create, { SetState } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
 interface PeopleState {
   people: string[];
   addPerson: (person: string) => void;
 }
 
-const useStore = create<PeopleState>((set) => ({
+const store = (set: SetState<PeopleState>) => ({
   people: ['hanumoka1', 'hanumoka2'],
-  addPerson: (person) =>
+  addPerson: (person: string) =>
     set((state) => ({
       people: [...state.people, person],
     })),
-}));
+});
+
+const store2 = devtools(store);
+const store3 = persist(store2, { name: 'people_settings' });
+
+const useStore = create(store3);
 
 export default useStore;
