92ee5b3352f05640e5df78c703451c1f523f4c64
amagramer <amagramer@gmail.com>
2022-05-13 19:45:49 +0900

    회원가입 모달 폼 수정

--- src/lib/modals/SignupModal.tsx
+++ src/lib/modals/SignupModal.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
+  Box,
   Button,
   FormControl,
   FormErrorMessage,
-  FormHelperText,
   FormLabel,
   Input,
   Modal,
@@ -14,95 +14,126 @@ import {
   ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
 
 interface signupProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+interface formError {
+  email?: string;
+  password?: string;
+  password2?: string;
+}
+
 const SignupModal = ({ isOpen, onClose }: signupProps) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setPassword(e.target.value);
-
-  const isEmailError = email === '';
-  const isPasswordError = password === '';
-
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
+
+  const closeModal = () => {
+    onClose();
+  };
+
   return (
-    <div>
+    <Box>
       <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
         isOpen={isOpen}
-        onClose={onClose}
+        onClose={closeModal}
       >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>회원가입</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl isInvalid={isEmailError}>
-              <FormLabel htmlFor="email">이메일</FormLabel>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                ref={initialRef}
-                placeholder="Email"
-              />
-              <FormHelperText>We'll never share your email.</FormHelperText>
-              {isEmailError && <FormErrorMessage>Email is required.</FormErrorMessage>}
-              {/*{!isEmailError ? (*/}
-              {/*  <FormHelperText>이메일을 입력하셔야 회원가입을 진행 할 수 있습니다.</FormHelperText>*/}
-              {/*) : (*/}
-              {/*  <FormErrorMessage>Email is required.</FormErrorMessage>*/}
-              {/*)}*/}
-            </FormControl>
-            <FormControl isInvalid={isPasswordError}>
-              <FormLabel htmlFor="password">비밀번호</FormLabel>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder="password"
-              />
-              {isPasswordError && <FormErrorMessage>password is required.</FormErrorMessage>}
-            </FormControl>
-            <FormControl isInvalid={isPasswordError}>
-              <FormLabel htmlFor="password">비밀번호 확인</FormLabel>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder="password"
-              />
-              {isPasswordError && <FormErrorMessage>password is required.</FormErrorMessage>}
-            </FormControl>
-          </ModalBody>
 
-          <ModalFooter>
-            <Button
-              colorScheme="blue"
-              mr={3}
-              onClick={() => {
-                alert('회원가입');
-              }}
-            >
-              회원가입
-            </Button>
-            <Button onClick={onClose}>취소</Button>
-          </ModalFooter>
+          <Formik
+            initialValues={{ email: '', password: '', password2: '' }}
+            validate={(values) => {
+              const errors: formError = {};
+
+              if (!values.email) {
+                errors.email = '이메일을 입력하세요.';
+              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = '이메일을 입력해주세요.';
+              }
+
+              if (!values.password) {
+                errors.password = '비밀번호를 입력하세요.';
+              } else if (!/^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/i.test(values.password)) {
+                errors.password = '8 ~ 16자 영문, 숫자 조합의 비밀번호를 입력해주세요.';
+              }
+
+              if (!values.password2) {
+                errors.password2 = '확인용 비밀번호를 입력하세요.';
+              } else if (
+                values.password &&
+                values.password2 &&
+                values.password !== values.password2
+              ) {
+                errors.password2 = '비밀번호와 확인용 비밀번호가 일치하지 않습니다.';
+              }
+
+              // 벨리데이션에서 빈 객체를 리턴할 경우 submit을 허용한다.
+              return errors;
+            }}
+            onSubmit={(values, actions) => {
+              console.log('회원가입 제출');
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                actions.setSubmitting(false);
+              }, 1000);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <ModalBody pb={6}>
+                  <Field name="email">
+                    {({ field, form }) => (
+                      <FormControl isInvalid={form.errors.email && form.touched.email}>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <Input {...field} id="email" placeholder="이메일" type="email" />
+                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {({ field, form }) => (
+                      <FormControl isInvalid={form.errors.password && form.touched.password}>
+                        <FormLabel htmlFor="password">비밀번호</FormLabel>
+                        <Input {...field} id="password" placeholder="비밀번호" type="password" />
+                        <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password2">
+                    {({ field, form }) => (
+                      <FormControl isInvalid={form.errors.password2 && form.touched.password2}>
+                        <FormLabel htmlFor="password2">비밀번호 확인</FormLabel>
+                        <Input
+                          {...field}
+                          id="password2"
+                          placeholder="비밀번호 확인"
+                          type="password"
+                        />
+                        <FormErrorMessage>{form.errors.password2}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </ModalBody>
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} isLoading={props.isSubmitting} type="submit">
+                    회원가입
+                  </Button>
+                  <Button onClick={closeModal}>취소</Button>
+                </ModalFooter>
+              </Form>
+            )}
+          </Formik>
         </ModalContent>
       </Modal>
-    </div>
+    </Box>
   );
 };
 
